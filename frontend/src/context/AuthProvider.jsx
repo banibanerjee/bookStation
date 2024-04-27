db69892cbec9910fa15bb4e652a2be9081087816
banibanerjee <banibanerjee09@gmail.com>
2024-04-27 19:33:19 +0530

    Backend Done!!

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const initialAuthUser = localStorage.getItem("Users");
+    const [authUser, setAuthUser] = useState(
+        initialAuthUser ? JSON.parse(initialAuthUser) : undefined
+    );
+
+    return (
+        <AuthContext.Provider value={[authUser, setAuthUser]}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider} from './context/AuthProvider';
+
+// Now you can use AuthProvider and useAuth in this file
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+   <AuthProvider>
    <div className='dark:bg-slate-900 dark:text-white'>
    <App />
    </div>
+   </AuthProvider>
   </BrowserRouter>,
 )
